0c128c4f330856002bab0a6c32dc6b782c6024d5
martin <martin.bartaa@gmail.com>
2024-10-10 18:09:11 +0300

    assignment 6

---
+++ newchart.js
@@ -0,0 +1,137 @@
+let birthData = [];
+let deathData = [];
+
+async function fetchBirthAndDeathData(municipalityCode) {
+    const url = 'https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px';
+    const query1 = {
+        "query": [
+            {
+                "code": "Vuosi",
+                "selection": {
+                    "filter": "item",
+                    "values": [
+                        "2000", "2001", "2002", "2003", "2004",
+                        "2005", "2006", "2007", "2008", "2009",
+                        "2010", "2011", "2012", "2013", "2014",
+                        "2015", "2016", "2017", "2018", "2019",
+                        "2020", "2021"
+                    ]
+                }
+            },
+            {
+                "code": "Alue",
+                "selection": {
+                    "filter": "item",
+                    "values": [municipalityCode]
+                }
+            },
+            {
+                "code": "Tiedot",
+                "selection": {
+                    "filter": "item",
+                    "values": ["vm01"]
+                }
+            }
+        ],
+        "response": {
+            "format": "json-stat2"
+        }
+    };
+    const query2 = {
+        "query": [
+            {
+                "code": "Vuosi",
+                "selection": {
+                    "filter": "item",
+                    "values": [
+                        "2000", "2001", "2002", "2003", "2004",
+                        "2005", "2006", "2007", "2008", "2009",
+                        "2010", "2011", "2012", "2013", "2014",
+                        "2015", "2016", "2017", "2018", "2019",
+                        "2020", "2021"
+                    ]
+                }
+            },
+            {
+                "code": "Alue",
+                "selection": {
+                    "filter": "item",
+                    "values": [municipalityCode]
+                }
+            },
+            {
+                "code": "Tiedot",
+                "selection": {
+                    "filter": "item",
+                    "values": ["vm11"]
+                }
+            }
+        ],
+        "response": {
+            "format": "json-stat2"
+        }
+    };
+
+    const births = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(query1),
+    });
+
+    const data = await births.json();
+    birthData = data.value;
+
+    const deaths = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(query2),
+    });
+
+    const data2 = await deaths.json();
+    deathData = data2.value;
+    createChart(birthData, deathData);
+}
+
+function createChart(birthData, deathData) {
+    const labels = [...Array(2022 - 2000).keys()].map(i => (2000 + i).toString());
+
+    const chart = new frappe.Chart("#chart", {
+        title: "Births and Deaths Data",
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    name: "Births",
+                    values: birthData,
+                    chartType: 'bar',
+                    color: '#63d0ff'
+                },
+                {
+                    name: "Deaths",
+                    values: deathData,
+                    chartType: 'bar',
+                    color: '#363636'
+                }
+            ]
+        },
+        type: 'bar',
+        height: 450,
+        colors: ['#63d0ff', '#363636'],
+        axisOptions: {
+            xAxisMode: 'tick',
+            yAxisMode: 'span',
+            xIsSeries: true
+        }
+    });
+}
+
+window.onload = function() {
+    const municipalityCode = localStorage.getItem('municipalityCode');
+    if (municipalityCode) {
+        fetchBirthAndDeathData(municipalityCode);
+    }
+};
---
+++ script.js
@@ -0,0 +1,124 @@
+let currentPopulationData = [];
+
+async function fetchPopulationData(municipalityCode = "SSS") {
+    const url = 'https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px';
+    const jsonQuery = {
+        "query": [
+            {
+                "code": "Vuosi",
+                "selection": {
+                    "filter": "item",
+                    "values": [
+                        "2000", "2001", "2002", "2003", "2004",
+                        "2005", "2006", "2007", "2008", "2009",
+                        "2010", "2011", "2012", "2013", "2014",
+                        "2015", "2016", "2017", "2018", "2019",
+                        "2020", "2021"
+                    ]
+                }
+            },
+            {
+                "code": "Alue",
+                "selection": {
+                    "filter": "item",
+                    "values": [municipalityCode]
+                }
+            },
+            {
+                "code": "Tiedot",
+                "selection": {
+                    "filter": "item",
+                    "values": ["vaesto"]
+                }
+            }
+        ],
+        "response": {
+            "format": "json-stat2"
+        }
+    };
+
+    const res = await fetch(url, {
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify(jsonQuery),
+    });
+
+    const data = await res.json();
+    const years = Object.keys(data.dimension.Vuosi.category.label);
+    const population = data.value;
+    currentPopulationData = population; 
+
+    createChart(years, population);
+}
+
+async function fetchMunicipalityCode(municipalityName) {
+    const url = 'https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px';
+    const res = await fetch(url);
+    const data = await res.json();
+    const codes = data.variables[1].values;
+    const names = data.variables[1].valueTexts;
+
+    const index = names.findIndex(name => name.toLowerCase() === municipalityName.toLowerCase());
+    return codes[index];
+}
+
+function addPredictedData() {
+    let deltas = [];
+    for (let i = 1; i < currentPopulationData.length; i++) {
+        deltas.push(currentPopulationData[i] - currentPopulationData[i - 1]);
+    }
+    const meanDelta = deltas.reduce((sum, delta) => sum + delta, 0) / deltas.length;
+    const predictedNextValue = currentPopulationData[currentPopulationData.length - 1] + meanDelta;
+    currentPopulationData.push(predictedNextValue);
+    const labels = [...Array(2022 - 2000).keys()].map(i => (2000 + i).toString());
+    labels.push("2022");
+
+    createChart(labels, currentPopulationData);
+}
+
+function createChart(labels, data) {
+    const chartElement = document.querySelector("#chart");
+    const chart = new frappe.Chart("#chart", {
+        title: "Data",
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    name: "Population",
+                    values: data,
+                }
+            ]
+        },
+        type: 'line',
+        height: 450,
+        colors: ['#eb5146'],
+        axisOptions: {
+            xAxisMode: 'tick',
+            yAxisMode: 'span',
+            xIsSeries: true
+        }
+    });
+}
+
+document.getElementById('search-form').addEventListener('submit', async function(event) {
+    event.preventDefault();
+    const municipalityName = document.getElementById('input-area').value;
+    const municipalityCode = await fetchMunicipalityCode(municipalityName);
+    if (municipalityCode) {
+        fetchPopulationData(municipalityCode);
+    }
+});
+
+document.getElementById('add-data').addEventListener('click', function() {
+    addPredictedData();
+});
+
+window.onload = function() {
+    fetchPopulationData();
+};
+
+if (municipalityCode) {
+    localStorage.setItem('municipalityCode', municipalityCode);
+    fetchPopulationData(municipalityCode);
+}
+
